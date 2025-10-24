1f976ac41fc9f55d6c57dea0fa91a33f57a7e385
albdelcig <albadelpino204@gmail.com>
2025-10-24 13:29:20 +0200

    lklñklñc

--- demo/demo/src/main/java/com/example/demo/PeluqueriaApplication.java
+++ demo/demo/src/main/java/com/example/demo/PeluqueriaApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PeluqueriaApplication {
 
-	public static void main([] args) {
+	public static void main(String[] args) {
 		SpringApplication.run(PeluqueriaApplication.class, args);
 	}
 
