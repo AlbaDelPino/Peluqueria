7672ea929a3e9bec3a092e0c05a9250da7f0ba6e
irecortas <ictjmb@gmail.com>
2025-12-05 13:57:55 +0100

    ?

--- demo/src/main/java/com/example/demo/security/service/TipoServicioServiceImpl.java
+++ demo/src/main/java/com/example/demo/security/service/TipoServicioServiceImpl.java
@@ -10,7 +10,6 @@ import java.util.Optional;
 
 @Service
 public class TipoServicioServiceImpl implements TipoServicioService {
-
     @Autowired
     private TipoServicioRepository tipoServicioRepository;
 
